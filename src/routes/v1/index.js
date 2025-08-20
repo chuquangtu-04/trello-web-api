5f6c799719b6bdaf91e40fcd9f757692c5d2922a
chu quang tu <chuquangtus2004@gmail.com>
2025-08-21 00:56:05 +0700

    Validation With Joi

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { StatusCodes } from 'http-status-codes'
-import { boardRouters } from './boardRoutes'
+import { boardRouter } from './boardRoute'
 const Router = express.Router()
 
 // Check Api v1.status
@@ -8,6 +8,6 @@ Router.get('/status', (req, res ) => {
   res.status(StatusCodes.OK).json({ message: 'you access about', code: StatusCodes.OK })
 })
 // Board APIs
-Router.use('/boards', boardRouters)
+Router.use('/boards', boardRouter)
 
 export const APIs_v1 = Router
