8631ae6e784b74f2e837c120b8b6c4be737e0218
chu quang tu <chuquangtus2004@gmail.com>
2025-08-22 16:53:50 +0700

    Complete Api Create

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-useless-catch */
 import { slugify } from '~/utils/formatter'
+import { boardModel } from '~/models/boardModel'
 
 const createNew = async (reqBody) => {
   try {
@@ -9,12 +10,16 @@ const createNew = async (reqBody) => {
       slug: slugify(reqBody.title)
     }
     // Gọi tới tầng model để xử lý lưu bản ghi newBoard vào database
+    const createBoard = await boardModel.createNew(newBoard)
+
+    // Lấy bản ghi board sau khi gọi (tùy mục đích dự án mà có cần bước này không)
+    const getNewBoard = await boardModel.findOneById(createBoard.insertedId)
 
     // Làm thêm các xử lý logic khác với các collection khác tùy đặc thù dự án
     // Bắn email, Notification về cho admin khi có 1 board mới được tạo
 
     // Trả kết quả về, trong service luôn phải có return 
-    return newBoard
+    return getNewBoard
   } catch (error) {throw error}
 }
 export const boardService = { createNew }
