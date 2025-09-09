f7954c5eb1be05d0e9d4d859fb6fd63fab80431b
chu quang tu <chuquangtus2004@gmail.com>
2025-09-09 16:05:37 +0700

    Update Whitelist Domains again

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 // Những domain được phép truy cập tới tài nguyên của server
 export const WHITELIST_DOMAINS = [
-  'https://trello-web-fullstack-xwrj.vercel.app/'
+  'https://trello-web-fullstack-xwrj.vercel.app'
 ]
 export const BOARD_TYPE = {
   'PUBLIC': 'public',
