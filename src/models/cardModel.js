e002428d70511ef860c3523f71c81b3fe2c82346
chu quang tu <chuquangtus2004@gmail.com>
2025-10-04 11:45:55 +0700

    Feature Card Members

--- src/models/cardModel.js
+++ src/models/cardModel.js
@@ -1,6 +1,7 @@
 import Joi from 'joi'
 import { ObjectId } from 'mongodb'
 import { GET_DB } from '~/config/mongodb'
+import { CARD_MEMBERS_ACTIONS } from '~/utils/constants'
 import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators'
 import { EMAIL_RULE, EMAIL_RULE_MESSAGE } from '~/utils/validators'
 
@@ -126,6 +127,32 @@ const unShiftNewComment = async (cardId, commentData) => {
     return result
   } catch (error) { throw new Error(error) }
 }
+const updateMembers = async (cardId, incomingMemberInfo) => {
+  try {
+    // Tạo ra một biến updateCondition ban đầu là rỗng
+    let updateCondition = {}
+
+    if (incomingMemberInfo.action === CARD_MEMBERS_ACTIONS.ADD) {
+      // console.log('Trường hợp Add, dùng $push:', incomingMemberInfo)
+      updateCondition = { $push: { memberIds: new ObjectId(incomingMemberInfo.userId) } }
+    }
+
+    if (incomingMemberInfo.action === CARD_MEMBERS_ACTIONS.REMOVE) {
+      // console.log('Trường hợp Remove, dùng $pull:', incomingMemberInfo)
+      updateCondition = { $pull: { memberIds: new ObjectId(incomingMemberInfo.userId) } }
+    }
+
+    const result = await GET_DB().collection(CARD_COLLECTION_NAME).findOneAndUpdate(
+      { _id: new ObjectId(cardId) },
+      updateCondition, // truyền cái updateCondition ở đây
+      { returnDocument: 'after' }
+    )
+
+    return result
+  } catch (error) { 
+    throw new Error(error) 
+  }
+}
 
 
 export const cardModel = {
@@ -136,5 +163,6 @@ export const cardModel = {
   updateCardOutColumn,
   hardDeleteCard,
   updateCard,
-  unShiftNewComment
+  unShiftNewComment,
+  updateMembers
 }
